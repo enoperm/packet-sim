8389338b7eb51fde705629b8f8ea352db0399bfe
Csaba Sarkadi <sarkadicsa@tutanota.de>
2021-04-19 17:10:44 +0200

    benchmarking: add scripts to make measurements reproducible

---
+++ benchmarks/bench-all.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+export RNG_SEED=42
+
+# invocation:
+# directory with txt files, used by GNU parallel to invoke the simulator,
+# followed by number of packets,
+# followed by a source of packet ranks.
+./bench-set.sh uniform 1000000 ../rank-sources/sawtooth.py
+./bench-set.sh geometric 1000000 ../rank-sources/geometric.py
---
+++ benchmarks/bench-set.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+set -xeo pipefail
+
+benchmark() {
+    local benchfile="${1}"
+    local outdir="${benchfile%/*}"
+    local outfile="${benchfile##/${outdir}}"
+    outfile="${outfile%.txt}.jsonl"
+
+    local packet_count="${2}"
+    local queue_count
+    local max_rank
+
+    shift 2
+
+    queue_count=$(grep -Po '\d+(?=q)' <<< "${benchfile}")
+
+    max_rank=$(grep -Po '\d+(?=r)' <<< "${benchfile}")
+    max_rank=$(( max_rank - 1 ))
+
+    ../eval-parallel.sh "${queue_count}" "${benchfile}" < <("${@}" "${queue_count}" "${packet_count}") > "${outfile}"
+}
+
+main() {
+    local defs_dir="${1:?must specify location of benchmark definitions}"
+    local packet_count="${2:?must specify packet count}"
+    shift 2
+
+    for benchfile in "${defs_dir}"/*.txt; do
+        benchmark "${benchfile}" "${packet_count}" "${*}"
+    done
+}
+
+main "${@}"
