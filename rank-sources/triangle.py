75cca558cce372ecb4e7eec92c898bbc56c877c1
Csaba Sarkadi <sarkadicsa@tutanota.de>
2021-05-02 20:51:04 +0200

    rename sawtooth source because it became a triangle one midway, but forgot to rename it

--- rank-sources/triangle.py
+++ rank-sources/triangle.py
@@ -7,9 +7,16 @@ max_rank = int(argv[1])
 packet_count = int(argv[2])
 
 generated = 0
+direction = 1
 head = 0
 
 while generated < packet_count:
     print(head)
-    head = (head + 1 % (max_rank + 1))
+    head += direction
+
+    if head == max_rank or head == 0:
+        print(head)  # to make distribution more uniform
+        generated += 1
+        direction *= -1
+
     generated += 1
