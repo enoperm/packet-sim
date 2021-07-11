a5938316e63474af467e886a22b6cb90e54cd647
Csaba Sarkadi <sarkadicsa@tutanota.de>
2021-07-11 14:14:22 +0200

    bench-all.sh: remove redundancy

--- benchmarks/bench-all.sh
+++ benchmarks/bench-all.sh
@@ -2,11 +2,12 @@
 
 export RNG_SEED=42
 export SAMPLE_INTERVAL=10000
+export PACKET_COUNT=1000000
 
 # invocation:
 # directory with txt files, used by GNU parallel to invoke the simulator,
 # followed by number of packets,
 # followed by a source of packet ranks.
-./bench-set.sh uniform   1000000 ../rank-sources/uniform.py
-./bench-set.sh triangle  1000000 ../rank-sources/triangle.py
-./bench-set.sh geometric 1000000 ../rank-sources/geometric.py
+./bench-set.sh uniform   ${PACKET_COUNT} ../rank-sources/uniform.py
+./bench-set.sh triangle  ${PACKET_COUNT} ../rank-sources/triangle.py
+./bench-set.sh geometric ${PACKET_COUNT} ../rank-sources/geometric.py
